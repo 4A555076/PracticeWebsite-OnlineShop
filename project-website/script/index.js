907fa43535031a11296cc9635141acbd8905fcdc
fangchen <fz555076@gmail.com>
2023-06-27 16:09:43 +0800

    fix: 修補輪播器

--- project-website/script/index.js
+++ project-website/script/index.js
@@ -1,76 +1,52 @@
-//slider
-const slideImages = document.querySelectorAll('img');
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev');
-const dots = document.querySelectorAll('.dot');
-
-
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 let counter = 0;
-next.addEventListener('click',slideNext)
-    function slideNext(){
-        slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
-        if(counter >= slideImages.length-1){
-            counter = 0;
-        }else{
-            counter++;
-        }
-        slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
-        indicators();
-    }
+let slideInterval;
 
-prev.addEventListener('click',slidePrev)
-function slidePrev(){
-    slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
-    if(counter == 0){
-        counter = slideImages.length-1;
-    }else{
-        counter--;
+function slide() {
+    setTimeout(slide, 7000);
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
-    indicators();
+    counter++;
+    if (counter > slides.length) {
+        counter = 1;
+    }
+    slides[counter - 1].style.display = "block";
 }
 
+slide();
 
-// 自動播放
-function autoSliding(){
-    deletInterval = setInterval(timer,3000);
-    function timer(){
-        slideNext();
-        indicators();
+// Previous button functionality
+prev.addEventListener("click", () => {
+    clearInterval(slideInterval); // Clear the automatic slide interval
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-}
-autoSliding();
-
-
-const container = document.querySelector('.slide-container');
-container.addEventListener('mouseover',function(){
-    clearInterval(deletInterval);
+    counter--;
+    if (counter < 1) {
+        counter = slides.length;
+    }
+    slides[counter - 1].style.display = "block";
 });
-container.addEventListener('mouseout',autoSliding);
 
-function indicators(){
-    for(i = 0 ; i < dots.length ; i++){
-        dots[i].className = dots[i].className.replace(' dot-active','');
-    }
-    dots[counter].className += ' dot-active';
-}
+// Next button functionality
+next.addEventListener("click", () => {
+    clearInterval(slideInterval); // Clear the automatic slide interval
 
-function switchImage(currentImage){
-    currentImage.classList.add('dot-active');
-    let imageId = currentImage.getAttribute('attr');
-    if(imageId>counter){
-        slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
-        counter = imageId;
-        slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
-    }else if(imageId == counter){
-        return;
-    }else{
-        slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
-        counter = imageId;
-        slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    indicators();
-}
+    counter++;
+    if (counter > slides.length) {
+        counter = 1;
+    }
+    slides[counter - 1].style.display = "block";
+});
+
+
 
 const style = document.createElement('style');
 const position = 'right';
